ba04e743bd627ddae2aba1a3220d4c79a66d9d04
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-03-10 08:26:16 +0530

    [Ansh] : Added a program of address book in feature/UC1-CreateContacts

--- src/test/java/com/addressbook/AddressbookApplicationTests.java
+++ src/test/java/com/addressbook/AddressbookApplicationTests.java
@@ -1,10 +1,11 @@
+
 package com.addressbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AddressbookApplicationTests {
+class AddressBookAppApplicationTests {
 
 	@Test
 	void contextLoads() {
